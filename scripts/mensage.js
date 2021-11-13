bce32c149f2e2381874dc0973dd4ef82b1921d68
Lucasb0 <lucasb.baptista2012@gmail.com>
2021-11-13 12:54:02 -0300

    Adicionando a pasta scripts

---
+++ scripts/mensage.js
@@ -0,0 +1,13 @@
+function mensage() {
+    const showmensage = [     
+        `
+        Nomes:
+
+        José Vinicius Pereira de Rezende,
+        Lucas Barros
+        
+        Turma: 3º TI
+        `
+    ]
+    window.alert(showmensage);
+}
